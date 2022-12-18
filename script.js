5b50c0a468be6899a423f18389ca9f86f8019f01
Sarah Richens <sarahgrichens@gmail.com>
2022-12-18 10:27:58 -0600

    add character length prompt and character confirms to generatePassword

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword() ; 
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
+function generatePassword() {
+  var passLength = prompt("How long? Between 8 and 128");
+  var charSet = confirm("Special characters? Press Ok for Yes or Cancel for No");
+  var num = confirm("Include numbers? Press Ok for Yes or Cancel for No");
+  var upper = confirm("Include uppercase? Press Ok for Yes or Cancel for No");
+  var lower = confirm("Include lowercase? Press Ok for Yes or Cancel for No");
+}
+  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
