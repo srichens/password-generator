81cb50914211270b3053a9bd1186d671e938e5b8
Sarah Richens <sarahgrichens@gmail.com>
2022-12-20 10:54:16 -0600

    put password lenght into separate function and character choices into separate function

--- script.js
+++ script.js
@@ -6,30 +6,81 @@ function writePassword() {
   var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+    
 }
 
 function generatePassword() {
+  
+  passPrompt();
+
+ 
+  /*let passLength = prompt("How long would you like your password to be? Choose a number between 8 and 128");
+  passLength = Number(passLength);*/
+
+  function passPrompt() {
   let passLength = prompt("How long would you like your password to be? Choose a number between 8 and 128");
-  let speChar = confirm("Special characters? Press Ok for Yes or Cancel for No");
+  passLength = Number(passLength);
+  }
+
+  /*let passLength = prompt("How long would you like your password to be? Choose a number between 8 and 128");
+  passLength = Number(passLength);*/
+
+  
+
+  charPrompt();
+
+
+
+  //if (passLength <= 7 || passLength >= 129 ) {prompt("Please choose a number between 8 and 128");} /*else if (passLength > 7 && passLength < 129) {charPrompt()};*/
+
+  /*if (passLength < 8) {prompt("Please choose a number between 8 and 128");}
+
+  else if (passLength > 129 ) {prompt("Please choose a number between 8 and 128");}*/
+
+  //console.log(passLength);
+  //if (passLength > 128) {wrongPassLength = prompt("Please choose a number between 8 and 128")};
+  
+  /*let speChar = confirm("Special characters? Press Ok for Yes or Cancel for No");
   let num = confirm("Include numbers? Press Ok for Yes or Cancel for No");
   let upper = confirm("Include uppercase? Press Ok for Yes or Cancel for No");
-  let lower = confirm("Include lowercase? Press Ok for Yes or Cancel for No");
-  let chosenPassLength = Number(passLength);
+  let lower = confirm("Include lowercase? Press Ok for Yes or Cancel for No");*/
+  
+  function charPrompt() {
+    let speChar = confirm("Special characters? Press Ok for Yes or Cancel for No");
+    let num = confirm("Include numbers? Press Ok for Yes or Cancel for No");
+    let upper = confirm("Include uppercase? Press Ok for Yes or Cancel for No");
+    let lower = confirm("Include lowercase? Press Ok for Yes or Cancel for No");
+
+  }
+
   const speCharValue = "$_!&%#@?<>";
   const numValue = "0123456789";
   const upperValue = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lowerValue = "abcdefghijklmnopqrstuvwxyz";
 
-  let chosenSpeChar = speCharValue[Math.floor(Math.random() * speCharValue.length)]
-  let chosenNum = numValue[Math.floor(Math.random() * numValue.length)]
-  let chosenUpper = upperValue[Math.floor(Math.random() * upperValue.length)]
-  let chosenLower = lowerValue[Math.floor(Math.random() * lowerValue.length)]
+  
+  
+  speChar = speCharValue[Math.floor(Math.random() * speCharValue.length)];
+  num = numValue[Math.floor(Math.random() * numValue.length)];
+  upper = upperValue[Math.floor(Math.random() * upperValue.length)];
+  lower = lowerValue[Math.floor(Math.random() * lowerValue.length)];
 
-  return chosenSpeChar + chosenNum + chosenUpper + chosenLower
+  /*for (let i = 0; i < passLength; i++) {}*/
+
+  
+  return speChar + num + upper + lower;
+}
+
+  /*let chosenSpeChar = speCharValue[Math.floor(Math.random() * speCharValue.length)];
+  let chosenNum = numValue[Math.floor(Math.random() * numValue.length)];
+  let chosenUpper = upperValue[Math.floor(Math.random() * upperValue.length)];
+  let chosenLower = lowerValue[Math.floor(Math.random() * lowerValue.length)];
+
+  return chosenSpeChar + chosenNum + chosenUpper + chosenLower*/
     
-   }
+   
 
-console.log(generatePassword());
+//console.log(generatePassword());
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
